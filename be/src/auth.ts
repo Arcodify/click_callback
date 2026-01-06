cd15b18ebf479a99e3b9da4d1ede63f4ed74eae6
Scelester <nabinpauudel664@gmail.com>
2026-01-07 00:11:29 +0545

    issuer v1 and v2 both

--- be/src/auth.ts
+++ be/src/auth.ts
@@ -9,7 +9,8 @@ declare module "fastify" {
 }
 
 const tenantId = env.AZURE_AD_TENANT_ID;
-const issuer = `https://login.microsoftonline.com/${tenantId}/v2.0`;
+const issuerV2 = `https://login.microsoftonline.com/${tenantId}/v2.0`;
+const issuerV1 = `https://sts.windows.net/${tenantId}/`;
 const jwks = createRemoteJWKSet(
   new URL(`https://login.microsoftonline.com/${tenantId}/discovery/v2.0/keys`)
 );
@@ -17,7 +18,7 @@ const jwks = createRemoteJWKSet(
 // Verify Azure AD access token signature + issuer + audience.
 export async function verifyAccessToken(token: string) {
   const { payload } = await jwtVerify(token, jwks, {
-    issuer,
+    issuer: [issuerV2, issuerV1],
     audience: env.AZURE_AD_API_AUDIENCE,
   });
   return payload;
